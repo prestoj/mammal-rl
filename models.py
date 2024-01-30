940c555068047fed1103688e0234269b9752b8a2
prestoj <prestonbjensen@gmail.com>
2024-01-30 00:02:30 -0700

    mim working

--- models.py
+++ models.py
@@ -6,8 +6,9 @@ from PIL import Image
 from timm.models.vision_transformer import VisionTransformer
 import random
 import torch
-from torchvision.datasets import MNIST
+from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
+from torch.utils.data import Subset
 
 class MIMVisionModel(nn.Module):
     def __init__(
@@ -19,15 +20,17 @@ class MIMVisionModel(nn.Module):
             device
         ):
         super(MIMVisionModel, self).__init__()
-        self.hideden_dim = hidden_dim
+        self.hidden_dim = hidden_dim
         self.num_heads = num_heads
         self.num_layers_encode = num_layers_encode
         self.num_layers_decode = num_layers_decode
         self.device = device
+        self.image_size = 32
+        self.patch_size = 4
 
         self.vision_model_encode = VisionTransformer(
-            img_size=224,
-            patch_size=16,
+            img_size=self.image_size,
+            patch_size=self.patch_size,
             embed_dim=hidden_dim,
             depth=num_layers_encode,
             num_heads=num_heads,
@@ -40,15 +43,15 @@ class MIMVisionModel(nn.Module):
 
         self.decode_mask_token = nn.Parameter(torch.rand((1, 1, hidden_dim)))
         self.vision_model_decode = VisionTransformer(
-            img_size=224,
-            patch_size=16,
+            img_size=self.image_size,
+            patch_size=self.patch_size,
             embed_dim=hidden_dim,
             depth=num_layers_decode,
             num_heads=num_heads,
             mlp_ratio=4,
             norm_layer=nn.LayerNorm,
             class_token=False,
-            num_classes=16*16*3,
+            num_classes=self.patch_size*self.patch_size*3,
             global_pool='map'
         )
         self.vision_model_encode.attn_pool = nn.Identity()
@@ -80,30 +83,40 @@ class MIMVisionModel(nn.Module):
 
         x = self.vision_model_encode.blocks(x)
         x = self.vision_model_encode.norm(x)
+        x = self.vision_model_encode.head(x)
+
         return x, mask
 
     def forward_decode(self, x, mask):
         B, N = mask.shape
 
         full_x = self.decode_mask_token.repeat(B, N, 1)
-        full_x[mask.unsqueeze(-1).repeat(1, 1, self.hideden_dim)] = x.flatten()
+        full_x[mask.unsqueeze(-1).repeat(1, 1, self.hidden_dim)] = x.flatten()
         x = full_x
+
+        x = self.vision_model_decode._pos_embed(x)
 
         x = self.vision_model_decode.blocks(x)
         x = self.vision_model_decode.norm(x)
 
         x = self.vision_model_decode.head(x)
 
-        x = x.view(B, 14, 14, 16, 16, 3)
-        x = x.permute(0, 5, 1, 3, 2, 4).contiguous()
-        x = x.view(B, 3, 224, 224)
-
         return x
     
     def forward(self, x, mask_ratio=0.5):
+        B, C, H, W = x.shape
         x, mask = self.forward_encode(x, mask_ratio)
         x = self.forward_decode(x, mask)
-        return x
+
+        x = x.view(B, self.image_size // self.patch_size, self.image_size // self.patch_size, self.patch_size, self.patch_size, 3)
+        x = x.permute(0, 5, 1, 3, 2, 4).contiguous()
+        x = x.view(B, 3, self.image_size, self.image_size)
+
+        mask = mask.reshape(B, mask.shape[1], 1, 1, 1).repeat(1, 1, self.patch_size, self.patch_size, 3).view(B, self.image_size // self.patch_size, self.image_size // self.patch_size, self.patch_size, self.patch_size, 3)
+        mask = mask.permute(0, 5, 1, 3, 2, 4).contiguous()
+        mask = mask.view(B, 3, self.image_size, self.image_size)
+
+        return x, mask
 
 if __name__ == "__main__":
     import torch.nn as nn
@@ -111,51 +124,72 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     transforms = transforms.Compose([
-        transforms.Resize((224, 224)), 
+        transforms.Resize((32, 32)),
+        # RandAugment(2, 9),
         transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,)),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.25, 0.25, 0.25)),
     ])
 
     # Define your training dataset
-    train_dataset = MNIST(root='./data', train=True, transform=transforms, download=True)
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-
+    train_dataset = CIFAR10(root='./data', train=True, transform=transforms, download=True)
+    train_loader = DataLoader(train_dataset, batch_size=1024, shuffle=True)
+    
     # Define your model
-    model = MIMVisionModel(hidden_dim=192, num_heads=3, num_layers_encode=6, num_layers_decode=2, device='cpu')
+    model = MIMVisionModel(hidden_dim=192, num_heads=3, num_layers_encode=6, num_layers_decode=6, device='cuda')
+    model = nn.DataParallel(model)
+    model = model.to('cuda')
+
+    # print out number of parameters in the model
+    print('Number of parameters:', sum(p.numel() for p in model.parameters() if p.requires_grad))
 
     # Define your loss function and optimizer
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    max_lr = 1e-3
+    warmup_steps = 10000
+    optimizer = optim.AdamW(model.parameters(), lr=0)
 
     plt.ion()
     fig, axes = plt.subplots(1, 2)
 
     # Training loop
-    num_epochs = 10
+    num_epochs = 1000
+    step = 0
     for epoch in range(num_epochs):
         for batch_idx, (images, _) in enumerate(train_loader):
-            images = images.repeat(1, 3, 1, 1)
+            step += 1
+            images = images.to('cuda')
             # Forward pass
-            reconstructions = model(images)
+            reconstructions, mask = model(images, mask_ratio=0.5)
 
             # Compute the loss
-            loss = criterion(reconstructions, images)
+            loss = criterion(reconstructions[~mask], images[~mask])
 
             print(f"Epoch [{epoch+1}/{num_epochs}], Step [{batch_idx+1}/{len(train_loader)}], Loss: {loss.item()}")
+
+            if step <= warmup_steps:
+                lr = max_lr * step / warmup_steps
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr
 
             # Backward pass and optimization
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            # Visualize
-            images = images * 0.3081 + 0.1307
-            reconstructions = reconstructions * 0.3081 + 0.1307
-            images = images.float().clamp(0, 1)
-            reconstructions = reconstructions.float().clamp(0, 1)
-            axes[0].imshow(images[0].permute(1, 2, 0))
-            axes[1].imshow(reconstructions[0].detach().permute(1, 2, 0))
-            plt.pause(0.01)
+            if step % 100 == 0:
+                # Visualize
+                images = images * 0.25 + 0.5
+                reconstructions = reconstructions * 0.25 + 0.5
+                images = images.float().clamp(0, 1)
+                reconstructions = reconstructions.float().clamp(0, 1)
+
+                # apply mask
+                images[~mask] = 0.5
+                reconstructions[mask] = 0.5
+
+                axes[0].imshow(images[0].cpu().permute(1, 2, 0))
+                axes[1].imshow(reconstructions[0].detach().cpu().permute(1, 2, 0))
+                plt.pause(0.1)
 
         print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
 
